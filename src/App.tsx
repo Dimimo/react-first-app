181ad995f352d708193a8e2bfff828cc3b4dbf13
Dimitri Mostrey <dmostrey@yahoo.com>
2025-09-19 23:14:40 +0800

    recipesList working with json structured data

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import {Header} from './Header.tsx'
 import {Footer} from "./Footer.tsx";
+import {Food} from "./Food.tsx";
 
 function App() {
     return (
         <>
             <Header/>
+            <Food/>
             <Footer/>
         </>
     );
---
+++ src/Food.tsx
@@ -0,0 +1,27 @@
+import {recipes} from './recipes.js';
+
+export function Food() {
+    return (
+        <div>
+            <h1>Recipes</h1>
+            {recipes.map(recipe =>
+                <Recipe {...recipe} key={recipe.id}/>
+            )}
+        </div>
+    );
+}
+
+function Recipe({name, ingredients}) {
+    return (
+        <div>
+            <h2>{name}</h2>
+            <ul>
+                {ingredients.map(ingredient =>
+                    <li key={ingredient}>
+                        {ingredient}
+                    </li>
+                )}
+            </ul>
+        </div>
+    );
+}
